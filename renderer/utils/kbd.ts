f37bb2b2f8326d9652e072061d59a1c31d32b1cc
livemehere <rhdxoals@gmail.com>
2025-03-03 18:06:47 +0900

    feat(renderer): utils

---
+++ renderer/utils/kbd.ts
@@ -0,0 +1,9 @@
+import { TPlatform } from '@shared/types/os-types';
+
+import { KBD_MAP } from '@/constants/kbd';
+
+export const resolveShortCutToKbd = (str: string, os: TPlatform) => {
+  return str.split('+').map<string>((key) => {
+    return (typeof KBD_MAP[key] === 'object' ? KBD_MAP[key][os] : KBD_MAP[key]) || key;
+  });
+};
