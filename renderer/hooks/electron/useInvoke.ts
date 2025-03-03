40da2d33282535995816ef3750fc7dabf81ffbc0
livemehere <rhdxoals@gmail.com>
2025-03-03 18:05:16 +0900

    feat(renderer): electron hook

---
+++ renderer/hooks/electron/useInvoke.ts
@@ -0,0 +1,34 @@
+import { ElectronBuddyInvokeMap, rendererIpc } from '@electron-buddy/ipc/renderer';
+import { useEffect, useState } from 'react';
+
+export default function useInvoke<
+  C extends keyof ElectronBuddyInvokeMap,
+  P extends ElectronBuddyInvokeMap[C]['payload']
+>(channel: C, payload: P) {
+  const [response, setResponse] = useState<ElectronBuddyInvokeMap[C]['response']>();
+  const [error, setError] = useState<Error>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const invoke = async () => {
+    setLoading(true);
+    try {
+      const response = await rendererIpc.invoke(channel, payload);
+      setResponse(response);
+    } catch (error) {
+      setError(error as Error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    invoke();
+  }, []);
+
+  return {
+    data: response,
+    error,
+    loading,
+    refetch: invoke
+  };
+}
---
+++ renderer/hooks/electron/useOn.ts
@@ -0,0 +1,22 @@
+import { rendererIpc, ElectronBuddyMessageMap } from '@electron-buddy/ipc/renderer';
+import { useEffect, useRef } from 'react';
+
+export default function useOn<C extends keyof ElectronBuddyMessageMap>(
+  channel: C,
+  listener: (response: ElectronBuddyMessageMap[C]['response']) => void,
+  deps: any[] = []
+) {
+  const listenerRef = useRef(listener);
+
+  useEffect(() => {
+    listenerRef.current = listener;
+  }, [listener]);
+
+  useEffect(() => {
+    const off = rendererIpc.on(channel, listenerRef.current);
+    return () => {
+      off();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [channel, ...deps]);
+}
