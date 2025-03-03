c18c222b60f26866a02f454f5e392ab46d659833
livemehere <rhdxoals@gmail.com>
2025-03-03 18:04:57 +0900

    feat(renderer): constants

---
+++ renderer/constants/kbd.ts
@@ -0,0 +1,29 @@
+export const KBD_MAP: {
+  [key: string]: string | { win32: string; darwin: string };
+} = {
+  Meta: '⌘',
+  Control: {
+    win32: 'Ctrl',
+    darwin: '⌃'
+  },
+  Alt: {
+    win32: 'Alt',
+    darwin: '⌥'
+  },
+  Shift: '⇧',
+  ArrowUp: '↑',
+  ArrowDown: '↓',
+  ArrowLeft: '←',
+  ArrowRight: '→',
+  Enter: '↵',
+  Escape: 'Esc',
+  Backspace: '⌫',
+  Delete: '⌦',
+  Tab: 'Tab',
+  CapsLock: 'Caps',
+  PageUp: 'PgUp',
+  PageDown: 'PgDn',
+  Insert: 'Ins',
+  PrintScreen: 'PrtScn',
+  ScrollLock: 'ScrLk'
+};
