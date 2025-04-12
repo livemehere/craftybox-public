80d57fcc5ea2e01d0e8cc6c70abece3a94b56058
livemehere <rhdxoals@gmail.com>
2025-04-13 02:32:14 +0900

    refactor: remove sidebar constants and related atom for cleaner codebase

--- renderer/stores/sideBarOpenAtom.ts
+++
@@ -1,4 +0,0 @@
-import { atomWithLocalStorage } from '@/stores/utils/atomWithLocalStorage';
-
-export const sideBarOpenAtom = atomWithLocalStorage('sideBarOpen', true);
-sideBarOpenAtom.debugLabel = 'sideBarOpen';
