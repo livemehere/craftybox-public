c32c0e825e28015aa81d9e18ea09b5005f7290db
livemehere <rhdxoals@gmail.com>
2025-04-13 22:50:39 +0900

    feat: implement ModuleManager class to handle module initialization and shortcut registration

---
+++ main/core/ModuleManager.ts
@@ -0,0 +1,72 @@
+import {
+  AppModule,
+  AppModuleClass,
+  TModuleShortcut,
+} from '@main/modules/AppModule';
+import { TUserShortcutSettings } from '@shared/types/shortcut-types';
+import { globalShortcut } from 'electron';
+import log from 'electron-log/main';
+import { STORE_KEY_MAP } from '@shared/constants';
+import { getStoreData } from '@shared/Store/main';
+
+import { App } from './App';
+
+const logger = log.scope('ModuleManager');
+
+export class ModuleManager {
+  moduleClasses: AppModuleClass[] = [];
+  instances: AppModule[] = [];
+  constructor(moduleClasses: AppModuleClass[]) {
+    this.moduleClasses = moduleClasses;
+  }
+
+  async initialize(app: App) {
+    const userSettings = getStoreData<TUserShortcutSettings>(
+      STORE_KEY_MAP.shortcuts,
+      []
+    );
+    const promises = [];
+
+    this.instances = this.moduleClasses.map((Module) => new Module(app));
+    logger.log(`${this.instances.length} modules detected.`);
+    for (const Module of this.instances) {
+      const promise = async () => {
+        await Module.initialize();
+        await Module.registerIpcHandlers();
+        this.registerModuleShortcuts(Module.getShortcuts(), userSettings);
+        logger.log(`${Module.name} initialized.`);
+      };
+      promises.push(promise());
+    }
+    await Promise.all(promises);
+  }
+
+  private registerModuleShortcuts(
+    shortcuts: TModuleShortcut[],
+    userSettings: TUserShortcutSettings
+  ): void {
+    for (const shortcut of shortcuts) {
+      const userSetting = userSettings.find((u) => u.key === shortcut.key);
+      const accelerator = userSetting?.value ?? shortcut.fallbackAccelerator;
+      const disabled = userSetting && !userSetting.enabled;
+      if (disabled) continue;
+      try {
+        globalShortcut.register(accelerator, shortcut.callback);
+        logger.log(
+          `${shortcut.key}(${accelerator}) : ${disabled ? 'disabled' : 'enabled'}`
+        );
+      } catch (error) {
+        logger.log(`Failed to change ${shortcut.key}: ${accelerator}`, error);
+      }
+    }
+  }
+
+  reRegisterShortcuts(userSettings: TUserShortcutSettings) {
+    globalShortcut.unregisterAll();
+    logger.log('Unregister all module shortcuts.');
+    for (const Module of this.instances) {
+      this.registerModuleShortcuts(Module.getShortcuts(), userSettings);
+    }
+    logger.log('Re-register module shortcuts.');
+  }
+}
