9eb1c44e9c22123bd09dedfa4b01ddd458ec957a
livemehere <rhdxoals@gmail.com>
2025-05-03 19:59:25 +0900

    refactor: remove unused getScaledSize function and clean up code

--- renderer/utils/size.ts
+++ renderer/utils/size.ts
@@ -15,27 +15,3 @@ export function getAspectRatio(
         height: height / gcdValue,
     };
 } 
-
-/**
- * 
- * @param width 
- * @param height 
- * @param scaleFactor 1 = 100%
- * @returns 
- */
-export function getScaledSize(
-  width: number,
-  height: number,
-  scaleFactor: number 
-): { width: number; height: number } {
-  const gcdValue = getGcd(width, height);
-  const _scaleFactor = Math.floor(scaleFactor * 10);
-  const scaledWidth = (width / gcdValue) * _scaleFactor;
-  const scaledHeight = (height / gcdValue) * _scaleFactor;
-  console.log(gcdValue, _scaleFactor, width/gcdValue);
-
-  return {
-    width: scaledWidth,
-    height: scaledHeight,
-  };
-}
