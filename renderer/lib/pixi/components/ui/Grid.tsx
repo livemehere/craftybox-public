3c41d786b76bf9364f10c1e3f99a56ac40927d0c
livemehere <rhdxoals@gmail.com>
2025-05-05 17:04:40 +0900

    feat: add infinite grid background component with dynamic scaling

---
+++ renderer/lib/pixi/components/ui/Grid.tsx
@@ -0,0 +1,85 @@
+import { Graphics } from 'pixi.js';
+
+import { usePixiEffect } from '@/lib/pixi/hooks/usePixiEffect';
+
+/**
+ * display infinite grid background
+ */
+const Grid = () => {
+  usePixiEffect((app) => {
+    const g = new Graphics();
+    g.alpha = 0.2;
+    g.label = 'grid-background';
+    app.stage.addChild(g);
+
+    // center point (0,0)
+    const centerPoint = new Graphics();
+    centerPoint.pivot.set(2.5, 2.5);
+    centerPoint.rect(0, 0, 5, 5).fill('red');
+    app.stage.addChild(centerPoint);
+
+    const centerLine = new Graphics();
+    centerLine.alpha = 0.5;
+    app.stage.addChild(centerLine);
+
+    const update = () => {
+      g.clear();
+      centerLine.clear();
+
+      const gridSize = Math.pow(
+        2,
+        Math.round(Math.log2(100 / app.stage.scale.x))
+      );
+      const viewWidth = app.screen.width / app.stage.scale.x;
+      const viewHeight = app.screen.height / app.stage.scale.y;
+
+      const offsetX = -app.stage.position.x / app.stage.scale.x;
+      const offsetY = -app.stage.position.y / app.stage.scale.y;
+
+      const startX = Math.floor(offsetX / gridSize) * gridSize;
+      const startY = Math.floor(offsetY / gridSize) * gridSize;
+      const endX = offsetX + viewWidth;
+      const endY = offsetY + viewHeight;
+
+      for (let x = startX; x <= endX; x += gridSize) {
+        g.moveTo(x, offsetY);
+        g.lineTo(x, endY);
+      }
+
+      for (let y = startY; y <= endY; y += gridSize) {
+        g.moveTo(offsetX, y);
+        g.lineTo(endX, y);
+      }
+
+      g.stroke({
+        pixelLine: true,
+        width: 1,
+        color: '#fff',
+      });
+
+      // center line
+      centerLine.moveTo(0, offsetY);
+      centerLine.lineTo(0, endY);
+      centerLine.moveTo(offsetX, 0);
+      centerLine.lineTo(endX, 0);
+
+      centerLine.stroke({
+        pixelLine: true,
+        width: 1,
+        color: '#fff',
+      });
+
+      centerPoint.scale.set(1 / app.stage.scale.x);
+    };
+
+    const ticker = app.ticker.add(update);
+
+    return () => {
+      ticker.destroy();
+      g.destroy();
+    };
+  });
+  return null;
+};
+
+export default Grid;
